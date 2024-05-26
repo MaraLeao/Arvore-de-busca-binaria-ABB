85fdb2bb6916f3c0ade27e286b7fa4d8dd3d669a
José Hermano <129210565+HermanoJr11@users.noreply.github.com>
2024-05-26 14:21:22 -0300

    arvore.c

--- arvore.c
+++ arvore.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
 typedef struct no {
@@ -8,39 +7,37 @@ typedef struct no {
 } NoArv;
 
 NoArv *criar() {
-    NoArv *no;
-    no = malloc(sizeof(NoArv));//altera o conte�do da raiz direto da main
-    no->valor = 0;
-    no->direita = NULL;
-    no->esquerda = NULL;
-
+    NoArv *no = (NoArv *)malloc(sizeof(NoArv));
+    if (no != NULL) {
+        no->valor = 0;
+        no->direita = NULL;
+        no->esquerda = NULL;
+    }
     return no;
-    
 }
 
 void inserir(NoArv **raiz, int num) {
-    
-    if(*raiz == NULL) {//verificando se o conteudo do ponteiro � nulo
+    if (*raiz == NULL) {
         *raiz = criar();
-        (*raiz)->valor = num;
-    }
-    else if ((*raiz)->valor != num){
-        if(num < (*raiz)->valor) {
-            inserir(&((*raiz)->esquerda), num); 
+        if (*raiz != NULL) {
+            (*raiz)->valor = num;
+        }
+    } else if ((*raiz)->valor != num) {
+        if (num < (*raiz)->valor) {
+            inserir(&((*raiz)->esquerda), num);
         } else {
             inserir(&((*raiz)->direita), num);
         }
-    } 
-    else {
-        printf("\n\tNo pode inserir numero repetido\n");
+    } else {
+        printf("\n\tNao pode inserir numero repetido\n");
     }
 }
 
 NoArv* buscar_v1(NoArv *raiz, int num) {
-    if(raiz) {
-        if (num == raiz->valor) 
+    if (raiz) {
+        if (num == raiz->valor)
             return raiz;
-        else if(num < raiz->valor)
+        else if (num < raiz->valor)
             return buscar_v1(raiz->esquerda, num);
         else
             return buscar_v1(raiz->direita, num);
@@ -48,15 +45,15 @@ NoArv* buscar_v1(NoArv *raiz, int num) {
     return NULL;
 }
 
-void mostrar_v2 (NoArv *raiz) { //mostra arvore ordenada
-    if(raiz) {
+void mostrar_v2(NoArv *raiz) {
+    if (raiz) {
         mostrar_v2(raiz->esquerda);
         printf("\t%d ", raiz->valor);
         mostrar_v2(raiz->direita);
     }
 }
 
-void esvaziar(NoArv **raiz) { //esvazia a arvore
+void esvaziar(NoArv **raiz) {
     if (*raiz != NULL) {
         esvaziar(&((*raiz)->esquerda));
         esvaziar(&((*raiz)->direita));
@@ -65,91 +62,135 @@ void esvaziar(NoArv **raiz) { //esvazia a arvore
     }
 }
 
-//Remover
 NoArv* remover(NoArv *raiz, int chave) {
     if (raiz == NULL) {
         printf("O valor não pode ser encontrado.\n");
         return NULL;
-    } else {
-        if (raiz->valor == chave) {
-            if (raiz->esquerda == NULL && raiz->direita == NULL) {
-                free(raiz);
-                printf("Folha removida: %d!\n", chave);
-                return NULL;
-            } else if (raiz->esquerda != NULL && raiz->direita != NULL) {
-                NoArv *pontaux = raiz->esquerda;
-                while (pontaux->direita != NULL) {
-                    pontaux = pontaux->direita;
-                }
-                raiz->valor = pontaux->valor;
-                raiz->esquerda = remover(raiz->esquerda, raiz->valor);
-                return raiz;
-            } else {
-                NoArv *temp = raiz->esquerda ? raiz->esquerda : raiz->direita;
-                free(raiz);
-                return temp;
+    }
+    if (raiz->valor == chave) {
+        if (raiz->esquerda == NULL && raiz->direita == NULL) {
+            free(raiz);
+            printf("Folha removida: %d!\n", chave);
+            return NULL;
+        } else if (raiz->esquerda != NULL && raiz->direita != NULL) {
+            NoArv *pontaux = raiz->esquerda;
+            while (pontaux->direita != NULL) {
+                pontaux = pontaux->direita;
             }
+            raiz->valor = pontaux->valor;
+            raiz->esquerda = remover(raiz->esquerda, pontaux->valor);
         } else {
-            if (chave < raiz->valor) {
-                raiz->esquerda = remover(raiz->esquerda, chave);
-            } else {
-                raiz->direita = remover(raiz->direita, chave);
-            }
-            return raiz;
+            NoArv *temp = raiz->esquerda ? raiz->esquerda : raiz->direita;
+            free(raiz);
+            return temp;
         }
+    } else if (chave < raiz->valor) {
+        raiz->esquerda = remover(raiz->esquerda, chave);
+    } else {
+        raiz->direita = remover(raiz->direita, chave);
+    }
+    return raiz;
+}
+
+void exibir_pre_ordem(NoArv *raiz) {
+    if (raiz) {
+        printf("%d ", raiz->valor);
+        exibir_pre_ordem(raiz->esquerda);
+        exibir_pre_ordem(raiz->direita);
     }
 }
 
-int main () {
+void exibir_in_ordem(NoArv *raiz) {
+    if (raiz) {
+        exibir_in_ordem(raiz->esquerda);
+        printf("%d ", raiz->valor);
+        exibir_in_ordem(raiz->direita);
+    }
+}
 
+void exibir_pos_ordem(NoArv *raiz) {
+    if (raiz) {
+        exibir_pos_ordem(raiz->esquerda);
+        exibir_pos_ordem(raiz->direita);
+        printf("%d ", raiz->valor);
+    }
+}
+
+int main() {
     NoArv *busca, *raiz = NULL;
-    int opcao,valor;
+    int opcao, valor;
 
     do {
-        printf("\n\t0 - Sair\n\t1 - Inserir\n\t2 - Imprimir\n\t3 - Buscar\n\t4 - Remover\n");
-        scanf("%d", &opcao);
+        printf("\n\t0 - Sair\n\t1 - Inserir\n\t2 - Imprimir\n\t3 - Buscar\n\t4 - Remover\n\t5 - Esvaziar\n\t6 - Exibir Pre-Ordem\n\t7 - Exibir Em-Ordem\n\t8 - Exibir Pos-Ordem\n");
+        if (scanf("%d", &opcao) != 1) {
+            printf("Erro na leitura da opcao\n");
+            continue;
+        }
 
         switch(opcao) {
             case 1:
                 printf("\n\tDigite um valor:");
-                scanf("%d",&valor);
-                inserir(&raiz, valor);//o & pega o endere�o da variavel raiz, como a variavel raiz � um ponteiro, da certinho com o parametro **raiz
+                if (scanf("%d", &valor) != 1) {
+                    printf("Erro na leitura do valor\n");
+                    continue;
+                }
+                inserir(&raiz, valor);
                 break;
             case 2:
-                printf("\n\tImpressao da arvore:\n");
+                printf("\n\tImpressao da arvore (em-ordem):\n");
                 mostrar_v2(raiz);
                 break;
             case 3:
                 printf("\n\tDigite um valor a ser pesquisado:");
-                scanf("%d", &valor);
+                if (scanf("%d", &valor) != 1) {
+                    printf("Erro na leitura do valor\n");
+                    continue;
+                }
                 busca = buscar_v1(raiz, valor);
-                         
-                if(busca) {
+                if (busca) {
                     printf("\n\tValor encontrado: %d\n", busca->valor);
                 } else {
-                    printf("\n\tValor no encontrado :/\n");
+                    printf("\n\tValor nao encontrado :/\n");
                 }
-
                 break;
-                
-            case 4:  
+            case 4:
                 printf("\t");
                 mostrar_v2(raiz);
-                printf("\n\tDigite o número que deseja remover: ");
-                scanf("%d", &valor);
-                raiz = remover(raiz,valor);
-                break;    
-                
+                printf("\n\tDigite o numero que deseja remover: ");
+                if (scanf("%d", &valor) != 1) {
+                    printf("Erro na leitura do valor\n");
+                    continue;
+                }
+                raiz = remover(raiz, valor);
+                break;
             case 5:
                 esvaziar(&raiz);
                 printf("\n\tArvore esvaziada!\n");
                 break;
+            case 6:
+                printf("\n\tExibindo em Pre-Ordem:\n");
+                exibir_pre_ordem(raiz);
+                printf("\n");
+                break;
+            case 7:
+                printf("\n\tExibindo em Em-Ordem:\n");
+                exibir_in_ordem(raiz);
+                printf("\n");
+                break;
+            case 8:
+                printf("\n\tExibindo em Pos-Ordem:\n");
+                exibir_pos_ordem(raiz);
+                printf("\n");
+                break;
             default:
                 if (opcao != 0) {
-                    printf("Digite uma opcao valida");
+                    printf("Digite uma opcao valida\n");
                 }
         }
     } while (opcao != 0);
+
+    // Limpeza da árvore antes de sair
+    esvaziar(&raiz);
 
     return 0;
 }
